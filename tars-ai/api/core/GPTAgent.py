6fffed9426707b0be92c75078acf1ff900ae40f3
dichill <dichilltomarong@yahoo.com>
2024-09-01 02:00:57 -0700

    added changes

---
+++ tars-ai/api/core/GPTAgent.py
@@ -0,0 +1,8 @@
+from logger import TarsLogger
+
+logging = TarsLogger()
+
+
+class GPTAgent:
+    def __init__(self):
+        pass
--- tars-ai/api/main.py
+++ tars-ai/api/main.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 from logger import TarsLogger
+import settings
 
 app = Flask(__name__)
 CORS(app)
 
 logging = TarsLogger()
+
+# Chat GPT
+# Voice Recognition
+# Text to Speech (?)
+
+
+# if __name__ == "__main__":
+#     app.run()
